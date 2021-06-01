7d6edf26976ac1ed3070c2f53ef39a9fa4865d53
felipe-spindola <felipeevalerio@outlook.com>
2021-06-01 10:01:55 -0300

    summary-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
@@ -5,8 +7,9 @@ export function App() {
   return (
     <>
       <Header/>
+      <Dashboard/>
       <GlobalStyle/>
-    </>
+    </> 
   );
 }
 
