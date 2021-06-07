fae7cfa53d6479999a679f8163a27f7386a5aff4
felipe-spindola <felipeevalerio@outlook.com>
2021-06-06 22:52:29 -0300

    buttons-functionality

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -52,31 +52,37 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: repeat(2,1fr);
     gap: 0.5rem;
 
-    button{
-        display: flex;
-        height:4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
-        background: transparent;
-        align-items: center;
-        justify-content: center;
-
-        transition: border-color 0.2s;
-
-        &:hover{
-            border-color: ${darken(0.1,"#d7d7d7")};
-        }
-
-        img{
-            height: 20px;
-            width: 20px;
-        }
-
-        span{
-            display: inline-block;
-            margin-left: 1rem;
-            font-size:1rem;
-            color: var(--text-title);
-        }
-    }
 `;
+
+
+interface RadioBoxProps{
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    display: flex;
+    height:4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    background: ${({isActive}) => isActive ? "#ccc" : "transparent"};
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover{
+        border-color: ${darken(0.1,"#d7d7d7")};
+    }
+
+    img{
+        height: 20px;
+        width: 20px;
+    }
+
+    span{
+        display: inline-block;
+        margin-left: 1rem;
+        font-size:1rem;
+        color: var(--text-title);
+    }
+`
