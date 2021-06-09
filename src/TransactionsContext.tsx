b694029e0398ac1348fb20e5b7181ce680730bcd
felipe-spindola <felipeevalerio@outlook.com>
2021-06-08 23:46:08 -0300

    Showing Transaction posted

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -25,7 +25,7 @@ type TransactionInput = Omit<Transaction,'id' | 'createdAt'>
 
 interface TransactionsContextData{
     transactions:Transaction[];
-    createTransaction:(transaction:TransactionInput) => void;
+    createTransaction:(transaction:TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -35,8 +35,16 @@ export const TransactionsContext = createContext<TransactionsContextData>(
 export function TransactionsProvider({children} : TransactionsProviderProps){
     const [transactions,setTransactions] = useState<Transaction[]>([])
 
-    function createTransaction(transaction:TransactionInput){
-        api.post("/transactions",transaction)
+    async function createTransaction(transactionInput:TransactionInput){
+        const response = await api.post("/transactions",{
+            ...transactionInput,
+            createdAt:new Date()
+        });
+        const { transaction } = response.data;
+        setTransactions([
+            ...transactions,
+            transaction
+        ]);
     }
     
     useEffect(() => {
